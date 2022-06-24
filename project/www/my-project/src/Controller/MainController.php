cb4ff2188dde2eea21a4ed66c21a4af153bb288a
Ludovic NAULOT <pctronique@free.fr>
2022-06-24 08:36:12 +0200

    ajout annonces en cours

---
+++ project/www/my-project/src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/mentions_legales", name="app_mentions")
+     */
+    public function mentions(): Response
+    {
+        return $this->render('main/mentions.html.twig');
+    }
+}
---
+++ project/www/my-project/src/Controller/UsersController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Annonces;
+use App\Form\AnnoncesType;
+use App\Form\EditProfileType;
+
+/**
+ * @Route("/users", name="users_")
+ */
+class UsersController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('users/index.html.twig', [
+            'controller_name' => 'UsersController',
+        ]);
+    }
+
+    /**
+     * @Route("/annonces/ajout", name="annonces_ajout")
+     */
+    public function ajoutAnnonce(Request $request): Response
+    {
+        $annonce = new Annonces;
+
+        $form = $this->createForm(AnnoncesType::class, $annonce);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $annonce->setUsers($this->getUsers());
+            $annonce->setActive(false);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($annonce);
+            $em->flush();
+
+            return $this->redirectToRoute('users_home');
+        }
+
+        return $this->render('users/annonces/ajout.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
